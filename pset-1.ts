b73a38d5f7e4eabcb9626e92b4085d12316ccc63
tmair@anonyome.com <tmair@anonyome.com>
2022-01-02 14:09:48 -0600

    pset-1

--- pset-1.ts
+++ pset-1.ts
@@ -1,12 +1,34 @@
-import { input, range } from "./utils";
+import { input } from "./utils";
+
+const portionDownPayment = 0.25;
+
+function monthlyInterest(currentSavings: number) {
+  const annualInterestRate = 0.04;
+  return (currentSavings * annualInterestRate) / 12;
+}
 
 (async () => {
-  const maybeX = await input("Enter number X: ");
-  const maybeY = await input("Enter number Y: ");
-  const x = parseInt(maybeX, 10);
-  const y = parseInt(maybeY, 10);
+  const maybeAnnualSalary = await input("Enter your annual salary: ");
+  const maybePortionSaved = await input(
+    "Enter the percent of your salary to save, as a decimal: "
+  );
+  const maybeTotalCost = await input("Enter the cost of your dream home: ");
 
-  for (const i of range(x, y)) {
-    console.log(i);
+  const annualSalary = parseFloat(maybeAnnualSalary);
+  const portionSaved = parseFloat(maybePortionSaved);
+  const totalCost = parseFloat(maybeTotalCost);
+
+  const monthlySalary = annualSalary / 12;
+  const salarySaved = monthlySalary * portionSaved;
+  const totalDown = totalCost * portionDownPayment;
+
+  function getMonths(currentSavings: number, months: number): number {
+    if (currentSavings >= totalDown) return months;
+    return getMonths(
+      currentSavings + monthlyInterest(currentSavings) + salarySaved,
+      months + 1
+    );
   }
+
+  console.log("Number of months: ", getMonths(0, 0));
 })();
